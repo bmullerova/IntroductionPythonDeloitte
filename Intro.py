3a891d1c152a610f745cc1964f516757b879b148
bmullerova <bmullerova@deloitte.com>
2017-04-14 11:16:22 +0200

    I've added Animal class to mymodule

--- Intro.py
+++ Intro.py
@@ -1,2 +1,6 @@
-print("Hello It's me")
-print("I've been wondering if after all this time you'd like to meet")
+import pandas as pd
+
+s = pd.Series([1,2,3])
+print(s)
+
+
---
+++ Mymodule.py
@@ -0,0 +1,9 @@
+class Animal(object):
+    def __init__(self, startname, age):
+        self.name = startname #attribute 1
+        self.age = age #attribute 2
+    def desctription(self): #method
+        print("this is " + self.name)
+        print("he.she is " + self.age + " years old")
+
+
